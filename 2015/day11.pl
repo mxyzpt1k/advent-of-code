b8158ce635d1458db13eb2ac8fbc668bf2eeba34
Dennis <98374387+mxyzpt1k@users.noreply.github.com>
2023-11-21 00:28:57 -0500

    adding a little perl

---
+++ 2015/day11.pl
@@ -0,0 +1,23 @@
+# day11.pl - advent of code 2015
+# Monday, November 20, 2023
+# warming up for 2023
+
+my $pass = "hxbxwxba";
+$pass = next_pass($pass);
+print "1) $pass\n";
+$pass = next_pass($pass);
+print "2) $pass\n";
+
+sub next_pass {
+    my $pass = shift;
+    while (1) {
+	$pass++;
+	$pass =~ /^[a-z]+$/ or next;
+	$pass =~ /[iol]/ and next;
+	$pass =~ /(.)\1.*(.)\2/ or next;
+	next if $1 eq $2;
+	$pass =~ /abc|bcd|cde|def|efg|fgh|ghi|hij|ijk|jkl|klm|lmn|mno|nop|opq|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz/ or next;
+	return $pass;
+    }
+}
+
