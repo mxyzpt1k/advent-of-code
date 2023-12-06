34b2f99f1ece9797b39dff86d6a90a0b71c83b54
Dennis <98374387+mxyzpt1k@users.noreply.github.com>
2023-12-06 00:55:37 -0500

    removed unused args

--- 2023/day06.el
+++ 2023/day06.el
@@ -6,7 +6,7 @@
 ;;; Day       Time   Rank  Score       Time   Rank  Score
 ;;;   6   00:25:28   8174      0   00:29:28   7023      0
 
-(defun day6-part-1 (buffer-name)
+(defun day6-part-1 ()
   (let ((times `(50     74     86     85))
 	(dists `(242   1017   1691   1252)))
     (seq-reduce #'* (cl-mapcar #'day6-count-wins times dists) 1)))
@@ -14,12 +14,12 @@
 (defun day6-part-2 (buffer-name)
   (day6-count-wins 50748685 242101716911252))
 
-(defun day6-count-wins (time record)
+(defun day6-count-wins ()
   (let ((wins 0))
     (dotimes (speed time wins)
       (when (> (* speed (- time speed)) record)
 	(setf wins (1+ wins))))))
 
-;; (aoc-copy-output () (day6-part-1 "day6.2023.input.txt"))
-;; (aoc-copy-output () (day6-part-2 "day6.2023.input.txt"))
+;; (aoc-copy-output () (day6-part-1))
+;; (aoc-copy-output () (day6-part-2))
 
