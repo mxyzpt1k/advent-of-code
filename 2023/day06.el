9e68b256b3342226b43fe7ddcaeee7ecb09c07f4
Dennis <98374387+mxyzpt1k@users.noreply.github.com>
2023-12-06 12:04:44 -0500

    day 6 optimizations

--- 2023/day06.el
+++ 2023/day06.el
@@ -24,3 +24,35 @@
 ;; (benchmark-run (aoc-copy-output () (day6-part-2)))
 ;;  => 84 seconds
 
+(defun day6-count-wins-afterwards (time record)
+  ;; turns out we're done when we find the first win because the
+  ;; solutions are symmetrical
+  ;;   d = v(t - v)
+  (catch 'wins
+    (dotimes (speed time)
+      (let ((time-left (- time speed)))
+	(when (> (* speed time-left) record)
+	  (throw 'wins (1+ (- time-left speed))))))))
+
+;; (benchmark-run (aoc-copy-output () (day6-count-wins-afterwards 50748685 242101716911252)))
+;;  => 9 seconds
+
+(defun day6-count-wins-afterwards2 (time record)
+  ;; Even faster, we can use the quadratic formula since
+  ;;   d = v(t - v) = tv - v^2
+  ;;   v^2 - tv + d = 0
+  ;; the two roots should be both close to the solution
+  (let ((a 1)
+	(b time)
+	(c record))
+    (cl-flet ((qf (p/m) (- (/ (funcall p/m (- b) (sqrt (- (* b b) (* 4 a c)))) (* 2 a))))
+	      (tst (v) (* v (- time v))))
+      (let* ((v (- (floor (qf #'+)) 4)) ; the smaller root + some margin
+	     (d (tst v)))
+	(while (<= d record)
+	  (setq v (1+ v))
+	  (setq d (tst v)))
+	(1+ (- (- time v) v))))))
+
+;; (benchmark-run (day6-count-wins-afterwards2 50748685 242101716911252)  )
+;;  => 5.4e-5 seconds
