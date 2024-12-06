7014bdcca0089fe5924c9e773b0a01430c1ab24d
Dennis <98374387+mxyzpt1k@users.noreply.github.com>
2024-12-06 02:05:39 -0500

    Update day6.jl
    Added comments

--- 2024/day6.jl
+++ 2024/day6.jl
@@ -1,12 +1,23 @@
+# Advent of Code
+# December 6, 2025
+
+# This was originally created in a Jupyter notebook, so some of the
+# lonely variables are the output of cells.
+
+# For part 1, I decided to rotate the map instead of keeping track
+# of direction when the guard turned right. That took more time than
+# I thought to get right.
+
 #       --------Part 1--------   --------Part 2--------
 # Day       Time   Rank  Score       Time   Rank  Score
 #   6   01:25:57  12173      0   01:48:07   6770      0
+
 
 f = open("day6.2024.input.txt")
 #f = open("day6.test")
 lines = readlines(f)
 close(f)
-Original = reverse(rotr90(stack(lines)), dims=2)
+Original = reverse(rotr90(stack(lines)), dims=2)  # bleah
 A = copy(Original);
 
 rows,cols = size(A)
@@ -18,7 +29,7 @@ for r =1:rows
         end
     end
 end
-start
+start   # cell output
 
 ## part 1
 
@@ -33,7 +44,7 @@ function solve(A,start)
     rows,cols=size(A)
     p = start
     k = 0
-    while k < 100000
+    while k < 100000  # arbitrary limit, but it seemed like enough
         k += 1
         row, col = p
         f = A[row-1,col]
@@ -62,9 +73,9 @@ for row = 1:rows
         end
     end
 end
-n+1
+n+1  # cell output and the answer to part 1
 
-## part 2
+## part 2  (simple brute force)
 
 rows, cols = size(A)
 n = 0
@@ -80,6 +91,6 @@ for row = 2:rows-1
         end
     end
 end
-n
+n  # cell output and the answer to part 2
 
 
