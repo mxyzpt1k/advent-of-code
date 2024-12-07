a2c7f0edf9ec429bd157fb27751e4081c8956b90
Dennis <98374387+mxyzpt1k@users.noreply.github.com>
2024-12-07 12:04:19 -0500

    Create day7.scm

---
+++ 2024/day7.scm
@@ -0,0 +1,32 @@
+;;; Saturday, December 07, 2024
+;;; Advent of Code
+
+;;; This relies on modifying the input data with sed
+;;; sed 's/://; s/^/(/; s/$/)/' ~/Downloads/day7.2024.input.txt > input.7
+
+(define (possible? ops target value nums)
+  (cond ((< target value) #f)
+	((null? nums) (= target value))
+	(else
+	 (let loop ((fs ops))
+	   (cond ((null? fs) #f)
+		 ((possible? ops target ((car fs) value (car nums)) (cdr nums)) #t)
+		 (else (loop (cdr fs))))))))
+
+(define (day7-part1 numbers)
+  (possible? (list + *) (car numbers) (cadr numbers) (cddr numbers)))
+
+(define (day7-part2 numbers)
+  (define (concat a b)
+    (let ((p (inexact->exact (floor (+ 1 (/ (log b) (log 10)))))))
+      (+ b (* (expt 10 p) a))))
+  (possible? (list + * concat) (car numbers) (cadr numbers) (cddr numbers)))
+
+(define (solve part)
+  (let loop ((nums (read))
+	     (sum 0))
+    (cond ((eof-object? nums) sum)
+	  ((part nums) (loop (read) (+ sum (car nums))))
+	  (else (loop (read) sum)))))
+
+;;(with-input-from-file "input.7" (lambda () (solve day7-part2)))
