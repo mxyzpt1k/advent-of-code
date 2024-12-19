2a2509e76d9d30d5290a96cf26547ba17bcc0b83
Dennis <98374387+mxyzpt1k@users.noreply.github.com>
2024-12-19 02:49:36 -0500

    aoc 2024 day 19

---
+++ 2024/day19.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl
+# Advent of Code
+# Thursday, December 19, 2024
+
+# not 1998458231011913
+
+use strict;
+use warnings;
+use Memoize;
+
+memoize('part2');
+
+my @towels = ();
+my $possible = 0;		# for part 1
+my $regexp = '';
+my @some_towels = ();
+my $combos = 0;			# for part 2
+
+$| = 1;
+
+while (<>) {
+  chomp;
+  if ($. == 1) {
+    @towels = split /, /;
+    my $s = join '|', @towels;
+    $regexp = qr(^($s)+$);
+  }
+  elsif ($. > 2) {
+    if (/$regexp/) {
+      $possible ++;
+      my $word = $_;
+      @some_towels = grep { index($word, $_) >= 0 } @towels;
+      $combos += part2( $_ );
+      print "$word  :: $combos\n";
+    }
+  }
+}
+
+print "$possible\n";
+print "$combos\n";
+
+sub part2 {
+  my $word = shift;
+  my $n = 0;
+  for (@some_towels) {
+    if (index($word, $_) == 0) {
+      if ($word eq $_) {
+	$n ++;
+      } else {
+	$n += part2(substr($word, length($_))); 
+      }
+    }
+  }
+  $n;
+}
