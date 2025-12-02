d2f7a126c93b98347da6ca11bdd27f9cbcbc47e3
Dennis <98374387+mxyzpt1k@users.noreply.github.com>
2025-12-01 20:07:04 -0500

    Update day1.pl
    Solving day 1, part 2 without another loop

--- 2025/day1.pl
+++ 2025/day1.pl
@@ -10,6 +10,9 @@ my $zeros = 0;			# part 1
 my $z2 = 0;			# part 2
 my $pos2 = 50;
 
+my $z3 = 0;			# afterwards
+my $pos3 = 50;
+
 while (<>) {
   chomp;
   s/R//;
@@ -25,7 +28,17 @@ while (<>) {
     $pos2 = ($pos2 + $sign) % 100;
     $z2 ++ unless $pos2;
   }
+
+  # afterwards - part 2 without another loop
+  $z3 += int(abs($_) / 100);
+  my $rem = $_ % 100 - (($_ < 0) ? 100 : 0);
+  my $sum = $pos3 + $rem;
+  $z3 ++ if $sum >= 100 or ($sum <= 0 and $pos3 > 0);
+  $pos3 = $sum % 100;
+
+  #printf "%5d :: $pos2: $z2  :: $pos3: $z3\n", $_;
 }
 
 print "$zeros\n";
 print "$z2\n";
+print "$z3\n";
